de3bee7ebb00751d35bc8b3213a0dfddd32daf10
shoei@live.com <shoei@live.com>
2022-03-16 15:42:08 +0000

    add stg_jaffle_shop test

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,9 +1,9 @@
 with customers as (
-    select * from {{ ref('stg_customers')}}
+    select * from {{ ref('stg_customers') }}
 ),
 
 orders as (
-    select * from {{ ref('fct_orders')}}
+    select * from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
